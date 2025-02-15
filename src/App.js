22da3114bed2a4cfea449466e3575daaf7156058
Christian Alfaro <135012673+deirUwU3@users.noreply.github.com>
2025-02-15 10:03:10 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Navbar from "./Navbar";
+
+export default function App() {
+  return (
+    <div class="text-center my-5">
+      <Navbar />
+      <h1>Christian Dale M. Alfaro</h1>
+      <p> Welcome to my page, My name is Christian Dale M. Alfaro.</p>
+      <p>I'm a 3rd year college student from Lyceum of Alabang taking BSIT.</p>
+    </div>
+  );
+}
